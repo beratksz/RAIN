0207b0f5dbb21de2b16d2e72c02bf4748f380b6e
Berat Öksüz <beratksz@icloud.com>
2025-02-16 16:46:23 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RAIN.Models;
+
+namespace RAIN.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult iletisim()
+        {
+            return View();
+        }
+
+        public IActionResult bireysel_ter()
+        {
+            return View(); 
+        }
+        public IActionResult kurumsal_ter()
+        {
+            return View();
+
+        }
+        public IActionResult ekibimiz()
+        {
+            return View();
+
+        }
+        public IActionResult aile_ter()
+        {
+            return View();
+        }
+        public IActionResult cift_ter()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RAIN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
